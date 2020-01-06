99e6a7faf45ddffc5d5699189209c42a46a69af3
Shashang <51124608+shashang29@users.noreply.github.com>
2020-01-06 22:26:19 +0545

    added weather display box

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, cloneElement } from 'react';
 
 
 
@@ -8,13 +8,18 @@ const SearchBox = () => {
         fetchWeather();
     }, []);
 
-
+    const [temperature, setTemperature] = useState([]);
 
     const fetchWeather = async () => {
         const data = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=fb07371e9e8f6a6c53f85498e92dcea1');
 
         const response = await data.json();
-        console.log(response.main);
+        const temperature = Object.entries(response.main)
+        temperature.length = 4;
+        console.log(temperature)
+        setTemperature(temperature);
+
+
     }
     return (
         <div>
@@ -27,7 +32,15 @@ const SearchBox = () => {
                     </button>
                 </div>
             </div>
+            <div className='center'>
+                <div className='weather-box mt2 pa3 f4 '>
+                    {temperature.map(item => (
+                        <p key={item[0]}>{item[0].toUpperCase()}: {parseFloat((item[1] - 273.15)).toFixed(1)} °C</p>
+                    ))}
+                </div>
+            </div>
         </div>
+
 
     )
 }
