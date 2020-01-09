b3488793628c54144488530843235f0977426cdb
Shashang <51124608+shashang29@users.noreply.github.com>
2020-01-09 20:20:48 +0545

    added map functionality

--- src/components/WeatherMap.js
+++ src/components/WeatherMap.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const WeatherMap = () => {
-    return (
-        <div>
-            <h1> Map</h1>
 
-        </div>
+    
+    const position = [28.505, 84.12];
+
+    return (
+        <Map className='map center mt5' center={position} zoom={2}>
+        <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+    />
+        <TileLayer
+          url="https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=fb07371e9e8f6a6c53f85498e92dcea1"
+          
+          
+        />
+        <Marker position={position}>
+          <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+        </Marker>
+      </Map>
     )
 }
 
