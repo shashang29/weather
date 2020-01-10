4dcb527fab2a301bcf2c73912d8c6facd8e0e092
Shashang <51124608+shashang29@users.noreply.github.com>
2020-01-10 16:49:47 +0545

    final client touch-up

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-<p className='f3'>This project has been made using react. The weather information is received from Open weather map API.
-The map is fetched using Leaflet library. </p>
+        <div className='about-box mt5 mr5 ml5 pa2'>
+            <h1 className='f1 ma2'>About</h1>
+            <p className='f2 pa5 pt3'>This project has been made using React library. 
+            The routing is handled by React router.
+            The weather information is received from Open weather map API. 
+            The map is fetched using Leaflet library and weather layers called from Open weather API. 
+            The stylying is done using CSS and tachyons framework. </p>
         </div>
     )
 }
