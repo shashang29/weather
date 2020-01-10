2eaf1f41ceddeb800cb14d3f64fa8a44551b8f88
Shashang <51124608+shashang29@users.noreply.github.com>
2020-01-10 13:46:05 +0545

    added map options

--- src/components/WeatherMap.js
+++ src/components/WeatherMap.js
@@ -13,11 +13,29 @@ const WeatherMap = () => {
     return (
         <div>
             <h2>Displaying map for:</h2>
-            <input list='options'
-                placeholder='Select an option' className='f4 tc pt1'
-                onChange={handleChange}
-            />
-            <datalist id='options'><option  >Temperature</option></datalist>
+            <div className='center'>
+                <div class="hide-child absolute ba pa2 center">Select an option
+            <select id="types" class="w-100 db h2 f6 bg-near-white ba b--sliver gray" name="">
+                        <option value="">All Types</option>
+                        <option label="Music" value="2">Space Type
+  </option>
+                        <option label="Photography" value="12">Space Type
+  </option>
+                        <option label="Film" value="22">Space Type
+  </option>
+                        <option label="Visual Art" value="32">Space Type
+  </option>
+                        <option label="Performance" value="42">Space Type
+  </option>
+                        <option label="Workshop" value="52">Space Type
+  </option>
+                        <option label="Event" value="62">Space Type
+  </option>
+                        <option label="Co-Working" value="72">Space Type
+  </option>
+                    </select>
+                </div>
+            </div>
             <Map className='map center mt5' center={position} zoom={2}  >
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -25,15 +43,15 @@ const WeatherMap = () => {
 
                 />
 
-                {(displayMap)? <TileLayer
+                {(displayMap) ? <TileLayer
                     url="https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?&appid=fb07371e9e8f6a6c53f85498e92dcea1"
 
 
-                />:
-                null
+                /> :
+                    null
 
                 }
-               
+
                 <Marker position={position}>
                     <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
                 </Marker>
