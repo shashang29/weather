d28ea27f46fa1ab1d463631ae15d84f32e4a81fb
Shashang <51124608+shashang29@users.noreply.github.com>
2020-03-06 15:06:47 +0545

    performance improvement with react lazy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
-import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 //components
 import Nav from './components/Nav';
 import SearchBox from './components/SearchBox';
-import WeatherMap from './components/WeatherMap';
-import About from './components/About';
+const WeatherMap = lazy(() => import('./components/WeatherMap'));
+const About = lazy(() => import('./components/About'));
+
 
 function App() {
     return (
         <Router>
-        <div className="App" >
-            <Nav/>
-            <Switch/>
-            <Route exact path="/"  component={SearchBox} />
-            <Route path="/weathermap" component={WeatherMap}/>
-            <Route path="/about" component={About}/>
-            <Switch/>
-
-        </div>
+            <div className="App" >
+                <Nav />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Switch />
+                    <Route exact path="/" component={SearchBox} />
+                    <Route path="/weathermap" component={WeatherMap} />
+                    <Route path="/about" component={About} />
+                    <Switch />
+                </Suspense>
+            </div>
         </Router>
     );
 }
