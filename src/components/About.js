f4f74bd858d07901b66c2e70d86003cdb080cd6d
Shashang <51124608+shashang29@users.noreply.github.com>
2020-03-06 18:56:02 +0545

    changes in about page

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,8 @@ const About = () => {
                     The routing is handled by React router.
                     The weather information is received from Open weather map API.
                     The map is fetched using Leaflet library and weather layers called from Open weather API.
-                    The stylying is done using CSS and tachyons framework. </p>
+                    The stylying is done using CSS and tachyons framework. Route-based code splitting has been implemented using React lazy to improve page-load time facilitating
+performance optimization </p>
             </div>
 
         </div>
